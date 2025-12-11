888f8e73de7082da35c31f8d3394ec1e13241727
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-12-11 09:10:54 -0300

    fix

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,6 +1,5 @@
 'use client';
 import styles from './Hero.module.css';
-import Image from 'next/image';
 
 export default function Hero() {
   return (
@@ -22,20 +21,6 @@ export default function Hero() {
               <a href="#projects" className={styles.secondaryBtn}>
                 Ver Projetos
               </a>
-            </div>
-          </div>
-          <div className={styles.imageContainer}>
-            <div className={styles.imageBorder}>
-              <div className={styles.placeholder}>
-                <Image 
-                  src="/images/PD.jpeg" 
-                  alt="Foto de Pablo Delgado"
-                  width={350}
-                  height={350}
-                  priority
-                  style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-                />
-              </div>
             </div>
           </div>
         </div>
